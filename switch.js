3a23be12eaa9a1c5b8687a4705d2767d1b3e8714
ignaciosua.h <ignaciosua.h@gmail.com>
2021-01-25 21:22:40 -0600

    Switch example

---
+++ switch.js
@@ -0,0 +1,24 @@
+//How to create a switch statement in javascript
+//Remember we are using Code Runner Extension in VScode to Execute JS
+
+//Declare your variables!
+
+let valueToBeCompared = 1;
+
+switch (valueToBeCompared) {
+    case 1:
+        console.log("Case one!");
+    break;
+    case 2:
+        console.log("Case Two!");
+    break;
+    default:
+        console.log("if nothing matches will always execute this!");
+    break;
+}
+
+//Execute code Remember CTRL-ALT-N
+
+//Thanks for watching! 
+//Remember to Subscribe for more Javascript in 30 seconds!
+
